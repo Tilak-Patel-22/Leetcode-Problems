9a32a88e1a98e4f657545db5da1a5b3feeb7e34f
Tilak Patel <tilakpatela@gmail.com>
2026-02-22 14:47:37 +0530

    Time: 0 ms (100%), Space: 8.3 MB (27.41%) - LeetHub

---
+++ 0483-smallest-good-base/0483-smallest-good-base.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string smallestGoodBase(string n) {
+    long long num = stoll(n);
+    for (int m = 60; m >= 1; m--) {
+        long long k = pow(num, 1.0 / m);
+        if (k < 2) continue;
+        long long sum = 1, cur = 1;
+        for (int i = 1; i <= m; i++) {
+            cur *= k;
+            sum += cur;
+            if (sum > num) break;
+        }
+        if (sum == num)
+            return to_string(k);
+    }
+    return to_string(num - 1);
+}
+};
