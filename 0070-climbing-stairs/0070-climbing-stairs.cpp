0239ea0638d98c74756ee8a3f1bad1c25f216da6
Tilak Patel <tilakpatela@gmail.com>
2026-06-23 14:53:44 +0530

    Time: 0 ms (100%), Space: 7.8 MB (87.09%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n <= 2) return n;
-        
-        int prev2 = 1;
-        int prev1 = 2;
-        
-        for (int i = 3; i <= n; i++) {
-            int curr = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = curr;
+        if (n <= 2)
+            return n;
+        int a = 1;
+        int b = 2;
+        for (int i = 2; i < n; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
         }
-        
-        return prev1;
+        return b;
     }
 };
